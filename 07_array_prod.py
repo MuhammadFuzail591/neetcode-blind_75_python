98095b9780f43295c0aafe3256aa8eb6ddf02db1
Muhammad Fuzail <muhammadfuzail591@gmail.com>
2025-11-07 22:33:52 +0500

    Done array prod with my solution

--- 07_array_prod.py
+++ 07_array_prod.py
@@ -1,4 +1,3 @@
-
 def productExceptSelf(nums: list[int]) -> list[int]:
    res = [1] * len(nums)
 
@@ -13,6 +12,18 @@ def productExceptSelf(nums: list[int]) -> list[int]:
       postfix *= nums[i]
 
    return res
+
+# My solution
+# from functools import reduce
+# def productExceptSelf(nums: list[int]) -> list[int]:
+#    result_arr = []
+#    for num in range(len(nums)):
+#       arr2 = list(nums)
+#       arr2.pop(num)
+#       product = reduce(lambda acc, val: acc * val, arr2, 1)
+#       result_arr.append(product)
+
+#    return result_arr
 
 res = productExceptSelf([1,2,3,4])
 print(res)
