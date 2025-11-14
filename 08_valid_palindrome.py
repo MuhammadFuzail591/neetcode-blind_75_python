e19047a609a1ab12ab1c6296f7fc69035baf2235
Muhammad Fuzail <muhammadfuzail591@gmail.com>
2025-11-14 23:00:01 +0500

    Done valide palidrome

--- 08_valid_palindrome.py
+++ 08_valid_palindrome.py
@@ -1,2 +1,13 @@
 def isPalindrome(s: str) -> bool:
-   pass
+   
+   newStr = ("".join(filter(str.isalnum, s))).lower()
+   result_string = ""
+
+   for char in newStr:
+      result_string = char + result_string
+   
+   return result_string == newStr
+   
+
+
+isPalindrome("tab a cat")
