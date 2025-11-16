122556a182693e6e20d2909e0e6deb95051e3eb8
Muhammad Fuzail <muhammadfuzail591@gmail.com>
2025-11-16 23:36:58 +0500

    My O(n^3) Solution

---
+++ 09_3sum.py
@@ -0,0 +1,16 @@
+def threeSum(nums: list[int]) -> list[list[int]]:
+   result = []
+   
+   for i in range(len(nums)):
+      for j in range(i + 1,len(nums)):
+         for k in range(j +1, len(nums)):
+            if nums[i] + nums[j] + nums[k] == 0 and i!=j!=k:
+               result.append(sorted([nums[i],nums[j],nums[k]]))
+
+   unique_tuples = set(tuple(inner_list) for inner_list in result)
+   unique_list = [list(t) for t in unique_tuples]
+   
+   return unique_list
+
+threeSum([-1,0,1,2,-1,-4])
+
