db2f4e5fb0c5b0036f775278357cbf2a5a95acb9
Muhammad Fuzail <muhammadfuzail591@gmail.com>
2025-11-27 22:52:45 +0500

    Done move Zeroes

--- 12_move_zeroes.py
+++ 12_move_zeroes.py
@@ -8,4 +8,26 @@ def moveZeroes(nums:list[int]):
 
    print(nums)
 
-moveZeroes([2,0,4,0,9])
+
+def moveZeroes2(nums:list[int]):
+   nextNonZero = 0
+   for i in range(len(nums)):
+      if nums[i] != 0:
+         nums[nextNonZero],nums[i] = nums[i],nums[nextNonZero]
+         nextNonZero +=1
+
+
+def moveZeroes3(nums:list[int]):
+   sIndex =0
+
+   for i in range(len(nums)):
+      if nums[i] != 0:
+         nums[sIndex] = nums[i]
+         sIndex+=1
+   
+   for i in range(sIndex,len(nums)):
+      nums[i] =0
+
+   print(nums)
+
+moveZeroes3([2,0,4,0,9])
