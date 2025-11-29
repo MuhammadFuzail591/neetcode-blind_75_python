271ae226e0e649db40dbfc0a29bda3d42e90e0cd
Muhammad Fuzail <muhammadfuzail591@gmail.com>
2025-11-29 23:05:09 +0500

    Solved sort colors with pointer solution

--- 13_sort_colors.py
+++ 13_sort_colors.py
@@ -7,4 +7,24 @@ def sortColors(nums:list[int]):
    print(nums)
 
 
-sortColors([2,1,2,0,1,0,1,0,1])
+def sortColors2(nums:list[int]):
+
+   i = 0
+   left = 0
+   right = len(nums) -1
+
+   while i<=right:
+      if nums[i] == 0:
+         nums[i],nums[left] = nums[left], nums[i]
+         left +=1
+         i +=1
+      elif nums[i] == 2:
+         nums[i],nums[right] = nums[right], nums[i]
+         right -=1
+      else:
+         i +=1
+   
+   print(nums)
+
+
+sortColors2([2,1,2,0,1,0,1,0,1])
