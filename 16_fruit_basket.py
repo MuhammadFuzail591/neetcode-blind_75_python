77a819573b11ee90870b1064d4ce1fdc268e1569
Muhammad Fuzail <muhammadfuzail591@gmail.com>
2025-12-12 22:48:49 +0500

    Solved fruit baskets by sliding window

---
+++ 16_fruit_basket.py
@@ -0,0 +1,19 @@
+def fruit_into_baskets(fruits:list[int]):
+   start = 0
+   state = {}
+   max_fruit = 0
+
+
+   for end in range(len(fruits)):
+      state[fruits[end]] = state.get(fruits[end],0) +1
+
+      while len(state) > 2:
+         state[fruits[start]] -=1
+         if state[fruits[start]] == 0:
+            del state[fruits[start]]
+         start +=1
+
+
+      max_fruit = max(max_fruit,end - start + 1)
+
+   return max_fruit
