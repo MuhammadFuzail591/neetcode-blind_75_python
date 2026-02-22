d81a5489c62ea3551b09fbc158ba8b29f22ea315
Muhammad Fuzail <muhammadfuzail591@gmail.com>
2026-02-22 23:39:36 +0500

    Added intervals problem

---
+++ 21_insert_interval.py
@@ -0,0 +1,18 @@
+def insert(self, intervals: list[list[int]], newInterval: list[int]) -> list[list[int]]:
+        merged = []
+        i = 0
+        n = len(intervals)
+
+        while i < n and intervals[i][1] < newInterval[0]:
+            merged.append(intervals[i])
+            i += 1
+
+        while i < n and intervals[i][0] <= newInterval[1]:
+            newInterval[0] = min(intervals[i][0], newInterval[0])
+            newInterval[1] = max(intervals[i][1], newInterval[1])
+            i += 1
+            
+        merged.append(newInterval)
+        for j in range(i, n):
+            merged.append(intervals[j])
+        return merged
